06b836abd54901b90745d73715a20203113de83b
Hansen <wh415471768@gmail.com>
2022-01-11 19:11:06 +0800

    提交部分逻辑

--- src/extension.js
+++ src/extension.js
@@ -1,8 +1,27 @@
 function activate (content) {
-    goby.registerCommand('start', function (content) {
+
+    //获取系统函数
+    let config = goby.getConfiguration();
+    const fs = require('fs');
+    const os = require('os');
+    var cp = require('child_process');
+    let os_type = os.type();
+
+    //显示工具首页
+    goby.registerCommand('showView', function (content) {
+        //加载主页html
         let path = require('path');
         let url = path.join(__dirname,'./page/index.html');
-        goby.showIframeDia(url,'OneForAll',666,600);
+
+        //检测python环境和oneforall是否安装成功
+        let python_path = config["python3"]["default"];
+        alert(python_path);
+        let OneForAll_path = config["oneforall.py"]["default"];
+        alert(OneForAll_path);
+
+
+        //渲染页面
+        goby.showIframeDia(url,'OneForAll',800,600);
     });
 }
 
