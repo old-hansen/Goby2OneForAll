ca060df2d132a853947730b3b76443f611939e45
Hansen <wh415471768@gmail.com>
2022-02-11 16:58:21 +0800

    实现cmd调用和可视化逻辑，功能目前已达到可用水平

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,6 @@
 //校验域名格式
 function checkDomain(domain) {
-    // let reg = /^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$/;
-    let reg = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,62}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
+    let reg = /^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$/;
 
     let re = new RegExp(reg);
     alert('domain = ' + domain);
@@ -20,20 +19,19 @@ function startOneForAll(domain) {
     // 获取gobyAPI
     let config = parent.goby.getConfiguration();
 
-    var oneforall_path = config["oneforall.py"]["default"];
-    var oneforall_dir = oneforall_path.substring(0,oneforall_path.length - 12);
-    alert(oneforall_dir);
+    let oneforall_path = config["oneforall.py"]["default"];
+    let oneforall_dir = oneforall_path.substring(0,oneforall_path.length - 12);
 
-    return;
     
     //按照系统类型执行command
-    var cmd = config["python3"]["default"] + ' ' + config["oneforall.py"]["default"] + ' --target ' + domain + ' run';
-    console.log(os.type())
+    let cmd = config["python3"]["default"] + ' ' + config["oneforall.py"]["default"] + ' --target ' + domain + ' run';
+
+    alert(cmd);
+
     if (os.type() == 'Windows_NT') {
         //windows
-        cmd += ' && explorer ' + open_dir;
+        cmd =  'start cmd.exe /K ' + cmd;
         cp.exec(cmd);
-
     } else if (os.type() == 'Darwin') {
         //mac
         cmd += ' && open ' + open_dir
@@ -42,30 +40,29 @@ function startOneForAll(domain) {
         //Linux
         cp.exec(`bash -c "${cmd}"`)
     }
+
+    parent.goby.showSuccessMessage('OneForAll开始运行');
 }
 
 // 打开OneForAll的文件夹
-function openDir() {
+function openDir(path) {
     const os = parent.OneForAll.getOS();
     const cp = parent.OneForAll.getCP();
-    let path = parent.goby.getConfiguration()["oneforall.py"]["default"];
+    path = parent.goby.getConfiguration()["oneforall.py"]["default"];
 
     let len = path.length - 12;
     path = path.substring(0,len);
-    let cmd = '';
 
+    //按照系统不同打开文件夹
     if (os.type() == 'Windows_NT') {
         //windows
-        cmd = 'explorer ' + path;
-        cp.exec(cmd);
+        cp.exec('explorer ' + path);
     } else if (os.type() == 'Darwin') {
         //mac
-        cmd = 'open ' + path;
-        cp.exec(`osascript -e 'tell application "Terminal" to do script "${cmd}"'`)
+        cp.exec('open ' + path);
     } else if (os.type() == 'Linux') {
         //Linux 需要自行安装nautilus
-        cmd = `nautilus ` + path;
-        cp.exec(cmd);
+        cp.exec(`nautilus ` + path);
     }
 
 }
